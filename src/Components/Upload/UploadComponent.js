34e32c85cd1d83bf4ce48a40cac2803fc24b1ada
Pankaj Patil <pankajhanmant.patil@sjsu.edu>
2019-10-25 17:50:31 -0700

    updated upload Page

--- src/Components/Upload/UploadComponent.js
+++ src/Components/Upload/UploadComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
 import { message } from "antd/lib/index";
 import { Upload, Button, Icon, Input } from 'antd';
+import axios from 'axios';
 import reqwest from 'reqwest';
 
 import { history } from '../../Helper/history';
@@ -11,106 +12,73 @@ const {TextArea} = Input;
 
 class UploadComponent extends Component {
 
-
     state = {
-        fileList: [],
-        uploading: false,
-        value: '',
-    };
+        selectedFile: '',
+        loaded: 0,
+        value: ''
+    }
 
+
+    handleUpload = () => {
+        const data = new FormData()
+        data.append('file', this.state.selectedFile, this.state.selectedFile.name);
+        data.append('description', this.state.value);
+
+
+        console.log("Uploading... " + this.state.selectedFile.name);
+        axios
+            .post('http://localhost:3000/upload', data, {
+                onUploadProgress: ProgressEvent => {
+                    this.setState({
+                        loaded: (ProgressEvent.loaded / ProgressEvent.total * 100),
+                    })
+                },
+            })
+            .then(res => {
+                message.success("File Uploaded Sucessfully!")
+            }).catch(err => {
+            console.log(err);
+        })
+
+    }
+
+    handleselectedFile = event => {
+        this.setState({
+            selectedFile: event.target.files[0],
+            loaded: 0,
+        })
+    }
 
     onChange = ({target: {value}}) => {
         this.setState({value});
     };
 
-
-    handleUpload = () => {
-        const {fileList} = this.state;
-        const formData = new FormData();
-        fileList.forEach(file => {
-            formData.append('files[]', file);
-        });
-
-
-        formData.description = this.state.value;
-
-
-        this.setState({
-            uploading: true,
-        });
-
-        // You can use any AJAX library you like
-        reqwest({
-            url: 'http://localhost:3000/upload',
-            method: 'post',
-            processData: false,
-            data: formData,
-            success: () => {
-                this.setState({
-                    fileList: [],
-                    uploading: false,
-                });
-                message.success('upload successfully.');
-            },
-            error: () => {
-                this.setState({
-                    uploading: false,
-                });
-                message.error('upload failed.');
-            },
-        });
-    };
-
     render() {
-        const {uploading, fileList} = this.state;
-        const props = {
-            onRemove: file => {
-                this.setState(state => {
-                    const index = state.fileList.indexOf(file);
-                    const newFileList = state.fileList.slice();
-                    newFileList.splice(index, 1);
-                    return {
-                        fileList: newFileList,
-                    };
-                });
-            },
-            beforeUpload: file => {
-                this.setState(state => ({
-                    fileList: [file],
-                }));
-                return false;
-            },
-            fileList,
-        };
+
 
         return (
             <div>
                 <div className="uploadArea">
-                    <Upload {...props}>
-                        <Button>
-                            <Icon type="upload"/> Select File
-                        </Button>
-                    </Upload>
 
-                    <br/>
+
+                    <input type="file" name="" id="" onChange={this.handleselectedFile}/>
+
+                    <br/> <br/>
+
                     <TextArea
                         value={this.state.value}
                         onChange={this.onChange}
-                        disabled={fileList.length === 0}
                         placeholder="Description"
                         autoSize={{minRows: 3, maxRows: 5}}
                     />
 
+                    <br/> <br/>
 
                     <Button
                         type="primary"
-                        onClick={this.handleUpload}
-                        disabled={fileList.length === 0}
-                        loading={uploading}
-                        style={{marginTop: 16}}
-                    >
-                        {uploading ? 'Uploading' : 'Start Upload'}
-                    </Button>
+                        onClick={this.handleUpload}>Upload </Button>
+
+
                 </div>
             </div>
         );
