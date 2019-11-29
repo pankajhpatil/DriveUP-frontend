fb898c5fdc9b9689b8922319d535b4a0418f92e5
Pankaj Patil <55374522+pankajhpatil@users.noreply.github.com>
2019-11-28 17:47:07 -0800

    added Icons for instructor schedule details

--- src/Components/Instructor/iSchedule.js
+++ src/Components/Instructor/iSchedule.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
-import { Table, Divider, Modal } from 'antd';
+import { Table, Divider, Modal,Icon } from 'antd';
 import { message } from "antd/lib/index";
 import { history } from '../../Helper/history';
 import { RESTService } from '../Api/api.js'
@@ -54,36 +54,70 @@ class ISchedule extends Component {
             {
                 title: '(8am-10am)',
                 dataIndex: 'slot0810',
-            },
+                render: (text) => <div>
+                { text=="Y" ? <Icon type="check" style={{color: '#3ebd04' }} />
+                  : <Icon type="dash" />
+                }
+              </div>            },
             {
                 title: '(10am-12pm)',
                 dataIndex: 'slot1012',
+                render: (text) => <div>
+                { text=="Y" ? <Icon type="check" style={{color: '#3ebd04' }} />
+                  : <Icon type="dash" />
+                }
+              </div>
             },
             {
                 title: '(12pm-2pm)',
                 dataIndex: 'slot1214',
+                render: (text) => <div>
+                { text=="Y" ? <Icon type="check" style={{color: '#3ebd04' }} />
+                  : <Icon type="dash" />
+                }
+              </div>
             },
             {
                 title: '(2pm-4pm)',
                 dataIndex: 'slot1416',
+                render: (text) => <div>
+                { text=="Y" ? <Icon type="check" style={{color: '#3ebd04' }} />
+                  : <Icon type="dash" />
+                }
+              </div>
             },
             {
                 title: '(4pm-6pm)',
                 dataIndex: 'slot1618',
+                render: (text) => <div>
+                { text=="Y" ? <Icon type="check" style={{color: '#3ebd04' }} />
+                  : <Icon type="dash" />
+                }
+              </div>
             },
             {
                 title: '(6pm-8pm)',
                 dataIndex: 'slot1618',
+                render: (text) => <div>
+                { text=="Y" ? <Icon type="check" style={{color: '#3ebd04' }} />
+                  : <Icon type="dash" />
+                }
+              </div>
             },
             {
                 title: '(8pm-10pm)',
                 dataIndex: 'slot1820',
+                render: (text) => <div>
+                { text=="Y" ? <Icon type="check" style={{color: '#3ebd04' }} />
+                  : <Icon type="dash" />
+                }
+              </div>
             },
             {
                 title: 'Action',
                 dataIndex: 'sdate',
                 render: (text) => <div><a
-                    onClick={() => this.deleteISchedule(text)}>Delete</a></div>,
+                    onClick={() => this.deleteISchedule(text)}><Icon type="delete" style={{color: '#f00d0a' }} /></a></div>,
             },
 
         ];
